2e6c390750f28c4c5218708c058790928e75063d
Håkon Påkon <gaffateip@gmail.com>
2018-03-03 23:29:29 +0100

    First time error handling woohoo.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-use std::env;
-use std::fs;
-use std::io::Read;
+use std::fs::File;
+use std::io::{self, Read};
 use std::path::Path;
+use std::fmt;
+
+#[derive(Debug)]
+enum NineError {
+    Io(io::Error)
+}
+
+impl From<io::Error> for NineError {
+    fn from(err: io::Error) -> NineError {
+        NineError::Io(err)
+    }
+}
+
+impl fmt::Display for NineError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            NineError::Io(ref err) => err.fmt(f)
+        }
+    }
+}
 
 struct CPU {
     pc_reg: u16,
@@ -28,13 +47,50 @@ impl CPU {
     }
 }
 
-fn load_file<P: AsRef<Path>>(path: P) -> Vec<u8> {
-    let mut file = fs::File::open(&path).unwrap();
+struct PPU {
+    blah: u8
+}
+
+impl PPU {
+    fn new() -> PPU {
+        PPU {
+            blah: 0
+        }
+    }
+}
+
+struct NES {
+    cpu: CPU,
+    ppu: PPU
+}
+
+impl NES {
+    fn new() -> NES {
+        NES {
+            cpu: CPU::new(),
+            ppu: PPU::new()
+        }
+    }
+    
+    fn run(&self, program: Vec<u8>) {
+    }
+}
+
+struct ROM {
+    
+}
+
+fn load_nes_file<P: AsRef<Path>>(path: P) -> Result<Vec<u8>, NineError> {
+    let mut file = try!(File::open(&path));
     let mut file_buf = Vec::new();
-    file.read_to_end(&mut file_buf).unwrap();
-    file_buf
+    try!(file.read_to_end(&mut file_buf));
+    Ok(file_buf)
 }
 
 fn main() {
-    let file_buf = load_file("roms/zelda.nes");
+    let nes = NES::new();
+    match load_nes_file("roms/zelda.nes") {
+        Ok(rom) => nes.run(rom),
+        Err(err) => println!("Error: {}", err)
+    }
 }
