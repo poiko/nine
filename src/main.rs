6e12077c4577f6922d432f7409332a42bd71111a
Håkon Påkon <gaffateip@gmail.com>
2018-03-03 18:48:08 +0100

    Some actual code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
+struct CPU {
+    pc_reg: u16,
+    sp_reg: u8,
+    acc_reg: u8,
+    x_reg: u8,
+    y_reg: u8,
+    status_reg: u8
+}
+
+impl CPU {
+    fn new() -> CPU {
+        CPU {
+            pc_reg: 0,
+            sp_reg: 0,
+            acc_reg: 0,
+            x_reg: 0,
+            y_reg: 0,
+            status_reg: 0
+        }
+    }
+
+    fn run() {
+    }
+}
+
+fn load_file<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = fs::File::open(&path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    file_buf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_buf = load_file("roms/zelda.nes");
 }
